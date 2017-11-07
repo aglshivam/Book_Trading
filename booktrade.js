1243f644b2574765296872bfd71a2a2581fdd159
sagarthakur <sagarthakur@students.iiit.ac.in>
2017-11-08 00:22:12 +0530

    Add book completed

--- booktrade.js
+++ booktrade.js
@@ -19,44 +19,31 @@ app.post('/login', urlencodedParser, function (req, res) {
 
   //check in the DB if user present or not
   MongoClient.connect('mongodb://localhost:27017/db', (err, db) => {
-  if (err) {
-    return console.log('Unable to connect to MongoDB server');
-  }
-  console.log('Connected to MongoDB server for login request');
-  //check userid is present in DB or not
-  db.collection('userInfo').find({userid: req.body.userid1}).toArray().then((docs) => {
-    
-    if(docs.length){
-        console.log(docs[0].password)
-        if(docs[0].password==req.body.pass1){
-        	console.log('congratulation')
-        	//dis
-        	res.render('dashboard.hbs', {
-		    userName: req.body.userid1,
-		    currentYear: new Date().getFullYear()
-		  });
-
-        }else{
-        	console.log('password does not match')
-        }
-    }else{
-          console.log('Username does not exists')  
+    if (err) {
+      return console.log('Unable to connect to MongoDB server');
     }
+    console.log('Connected to MongoDB server for login request');
+    //check userid is present in DB or not
+    db.collection('userInfo').find({userid: req.body.userid1}).toArray().then((docs) => {
+      
+      if(docs.length){
+          console.log(docs[0].password)
+          if(docs[0].password==req.body.pass1){
+          	console.log('congratulation')
+          	res.render('dashboard.hbs', {
+  		    userName: req.body.userid1,
+  		    currentYear: new Date().getFullYear()
+  		  });
+
+          }else{
+          	console.log('password does not match')
+          }
+      }else{
+            console.log('Username does not exists')  
+      }
+    });
   });
-
-  // db.close();
 });
-
-
-
-
-  //res.send('welcome, ' + req.body.userid1)
- // res.sendFile(__dirname + '/dashboard.html');
-  // res.render('dashboard.hbs', {
-  //   userName: req.body.userid1,
-  //   currentYear: new Date().getFullYear()
-  // });
-})
 
 app.post('/signup', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
@@ -67,7 +54,7 @@ app.post('/signup', urlencodedParser, function (req, res) {
     if (err) {
       return console.log('Unable to connect to MongoDB server');
     }
-    console.log('Connected to MongoDB server for signup request');
+    console.log('Connected to MongoDB server for book add request');
 
     // Insert new doc into userInfo collection
     db.collection('userInfo').insertOne({
@@ -76,10 +63,10 @@ app.post('/signup', urlencodedParser, function (req, res) {
       state: req.body.state,
       password: req.body.pass,
       userid: req.body.userid2
-    }, (err, result) => {
-      if (err) {
-        return console.log('Unable to insert user', err);
-      }
+      }, (err, result) => {
+        if (err) {
+          return console.log('Unable to insert user', err);
+        }
 
       console.log(result.ops);
     });
@@ -91,17 +78,35 @@ app.post('/signup', urlencodedParser, function (req, res) {
     userName: req.body.userid2,
     currentYear: new Date().getFullYear()
   });
-
-
-})
-
-app.post("/addbooks", urlencodedParser, function (req, res) {
-    //var obj = JSON.parse(res)
-    console.log(req.body.title)
-    res.send('ok');
 });
 
+app.post("/addbooks", urlencodedParser, function (req, res) {
+      //DB connection for signup
+    MongoClient.connect('mongodb://localhost:27017/db', (err, db) => {
+      if (err) {
+        return console.log('Unable to connect to MongoDB server');
+      }
+      console.log('Connected to MongoDB server for signup request');
 
+      // Insert new doc into userInfo collection
+      db.collection('userInfo').insertOne({
+        userid: req.body.userid,
+        isbn: req.body.isbn,
+        title: req.body.title,
+        price: req.body.price,
+        author: req.body.author
+      }, (err, result) => {
+        if (err) {
+          return console.log('Unable to insert book', err);
+        }
+
+        console.log(result.ops);
+      });
+
+      db.close();
+    });
+    res.send("Book record added!")
+});
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
